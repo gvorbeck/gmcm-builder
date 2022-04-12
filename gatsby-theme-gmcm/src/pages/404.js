51fbe85d17f46372cb6d6fe34f5907ea2cc9db50
Garrett Vorbeck <gvorbeck@gmail.com>
2022-04-12 16:51:56 -0400

    404 page

--- gatsby-theme-gmcm/src/pages/404.js
+++ gatsby-theme-gmcm/src/pages/404.js
@@ -1,7 +1,24 @@
+/* eslint-disable react/jsx-indent */
 import React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
+import { Box, Divider, Paper, Typography } from '@mui/material';
+import Layout from '../components/layout';
+
+const Image = () => <StaticImage src="../images/404.png" alt="Sword" />
 
 function FourOhFourPage() {
-  return <h1>GMCM 404 Page</h1>;
+  return (
+    <Layout title="404!">
+      <Box>
+        <Paper sx={{ p: 2, textAlign: 'center' }}>
+          <Image />
+          <Divider sx={{ my: 2 }} />
+          <Typography>Your party has become lost in rough terrain.</Typography>
+          <Typography>Turn back now.</Typography>
+        </Paper>
+      </Box>
+    </Layout>
+  );
 }
 
 export default FourOhFourPage;
